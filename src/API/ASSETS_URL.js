1a309dc818e20eb2a4a69e5dc1e7306126d4109d
Marxie <jhomsabdulmukim@gmail.com>
2025-10-27 08:52:13 +0800

    Updated API

--- src/API/ASSETS_URL.js
+++ src/API/ASSETS_URL.js
@@ -1,5 +1,5 @@
 // const ASSETS_URL = "http://127.0.0.1:5000/"
 
-const ASSETS_URL = "http://localhost:5000/"
+const ASSETS_URL = "https://mit-portal-backend.onrender.com/"
 
 export default ASSETS_URL;
